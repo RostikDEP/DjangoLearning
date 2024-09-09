3dd72e7d415994b0a8e47f00071624820ce5289c
Stadnikov Rostyslav <stadnkikov2000@gmail.com>
2024-09-09 15:34:09 -0700

    Created urls.py for module UserProfile

--- NetSite/urls.py
+++ NetSite/urls.py
@@ -15,7 +15,7 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 
 import UserProfile.views
 from UserProfile.views import *
@@ -23,5 +23,5 @@ from UserProfile.views import *
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', index),
-    path('register/', register)
+    path('', include("UserProfile.urls"))
 ]
