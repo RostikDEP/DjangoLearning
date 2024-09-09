fff6d0938d6d36e09218829de57094642ee539bb
Stadnikov Rostyslav <stadnkikov2000@gmail.com>
2024-09-09 15:17:41 -0700

    Created firs view

--- NetSite/urls.py
+++ NetSite/urls.py
@@ -17,6 +17,10 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 
+import UserProfile.views
+from UserProfile.views import *
+
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', index)
 ]
